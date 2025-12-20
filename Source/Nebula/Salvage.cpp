f6f5a571ca4f176c550f18841cc355c306b4bea8
fantomtheghost <101781683+fantomtheghost1@users.noreply.github.com>
2025-12-20 15:52:48 -0700

    Implemented ship salvaging and backend improvements

---
+++ Source/Nebula/Salvage.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Salvage.h"
+
+// Sets default values
+ASalvage::ASalvage()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASalvage::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASalvage::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
