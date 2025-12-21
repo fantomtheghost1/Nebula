847155d325920d29ddc0aec6fecc2ff7242f48ae
fantomtheghost <101781683+fantomtheghost1@users.noreply.github.com>
2025-12-20 18:47:47 -0700

    Implemented a battlespace for fleet-to-fleet combat. Also implemented returning to the main playspace after the fleet is defeated.

---
+++ Source/Nebula/NebulaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NebulaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEBULA_API ANebulaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
