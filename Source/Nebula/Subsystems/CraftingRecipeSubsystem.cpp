71a0799b2b033ad916f8afe6af46222810275e92
fantomtheghost <101781683+fantomtheghost1@users.noreply.github.com>
2026-01-15 22:27:20 -0700

    Moved the crafting recipe loading into a game instance subsystem

---
+++ Source/Nebula/Subsystems/CraftingRecipeSubsystem.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CraftingRecipeSubsystem.h"
+
+#include "DataAssets/CraftingRecipeAsset.h"
+#include "Engine/AssetManager.h"
+
+void UCraftingRecipeSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	TArray<FAssetData> TempRecipes;
+	UAssetManager& AM = UAssetManager::Get();
+	AM.GetPrimaryAssetDataList(
+		FPrimaryAssetType("CraftingRecipe"),
+		TempRecipes
+		);
+	
+	for (const FAssetData& Recipe : TempRecipes)
+	{
+		UCraftingRecipeAsset* RecipeAsset = Cast<UCraftingRecipeAsset>(Recipe.GetAsset());
+		if (RecipeAsset)
+		{
+			Recipes.Add(RecipeAsset);
+		}
+	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Crafting Recipes Loaded: %d"), Recipes.Num());
+}
+
+TArray<UCraftingRecipeAsset*> UCraftingRecipeSubsystem::GetRecipes() const
+{
+	return Recipes;
+}
+
+UCraftingRecipeAsset* UCraftingRecipeSubsystem::GetRecipe(FName RecipeName)
+{
+	for (int i = 0; i < Recipes.Num(); i++)
+	{
+		if (Recipes[i]->Result == RecipeName) return Recipes[i];
+	}
+	
+	return nullptr;
+}
---
+++ Source/Nebula/Subsystems/CraftingRecipeSubsystem.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DataAssets/CraftingRecipeAsset.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CraftingRecipeSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEBULA_API UCraftingRecipeSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	UFUNCTION(BlueprintCallable)
+	TArray<UCraftingRecipeAsset*> GetRecipes() const;
+	
+	UFUNCTION(BlueprintCallable)
+	UCraftingRecipeAsset* GetRecipe(FName RecipeName);
+	
+private:
+	
+	TArray<UCraftingRecipeAsset*> Recipes;
+	
+};
