6f489f60914bcbe71a3c2b8376f856fbbe96bfa0
fantomtheghost <101781683+fantomtheghost1@users.noreply.github.com>
2026-02-20 20:39:31 -0700

    Minor change to asteroids

--- Source/Nebula/Objects/Asteroid.cpp
+++ Source/Nebula/Objects/Asteroid.cpp
@@ -10,6 +10,8 @@ AAsteroid::AAsteroid()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	Tags.Add(FName(TEXT("Asteroid")));
 
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 
